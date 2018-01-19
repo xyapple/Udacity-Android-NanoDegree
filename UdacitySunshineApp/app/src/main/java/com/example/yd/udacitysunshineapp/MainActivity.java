1aad8158bff7cbe97e92ff56c80710fb01bd7e0d
xyapple <ydeascentis@gmail.com>
2018-01-18 22:13:15 -0500

    add a reflesh button on the Menu

--- UdacitySunshineApp/app/src/main/java/com/example/yd/udacitysunshineapp/MainActivity.java
+++ UdacitySunshineApp/app/src/main/java/com/example/yd/udacitysunshineapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.yd.udacitysunshineapp;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.example.yd.udacitysunshineapp.data.SunshinePreferences;
@@ -79,6 +82,33 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    //Create a menu resource in res/menu/ called forecast.xml
+    //Add one item to the menu with an ID of action_refresh
+    //Set the title of the menu item to "Refresh" using strings.xml
+    //Override onCreateOptionsMenu to inflate the menu for this Activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        /* Use AppCompatActivity's method getMenuInflater to get a handle on the menu inflater */
+        MenuInflater inflater = getMenuInflater();
+        /* Use the inflater's inflate method to inflate our menu layout to this menu */
+        inflater.inflate(R.menu.forecast, menu);
+        /* Return true so that the menu is displayed in the Toolbar */
+        return true;
+    }
+    //Override onOptionsItemSelected to handle clicks on the refresh button
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            mWeatherTextView.setText("");
+            loadWeatherData();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
