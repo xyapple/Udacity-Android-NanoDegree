ae0c98a5007b8a5c2652f2fa0120e5ae7931da3a
xyapple <ydeascentis@gmail.com>
2018-01-26 14:44:48 -0500

    added the menu button T03.06-Exercise-RefreshMenuButton

--- ud851-Exercises-student/Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ ud851-Exercises-student/Lesson03-Green-Recycler-View/T03.06-Exercise-RefreshMenuButton/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -79,9 +79,33 @@ public class MainActivity extends AppCompatActivity {
     // TODO (7) Override onCreateOptionsMenu
     // TODO (8) Use getMenuInflater().inflate to inflate the menu
     // TODO (9) Return true to display this menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
     // TODO (10) Override onOptionsItemSelected
     // TODO (11) Within this method, get the ID from the MenuItem
     // TODO (12) If the ID equals R.id.action_refresh, create and set a new adapter on the RecyclerView and return true
     // TODO (13) For now, for all other IDs, return super.onOptionsItemSelected
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int itemId = item.getItemId();
+
+        switch (itemId) {
+            /*
+             * When you click the reset menu item, we want to start all over
+             * and display the pretty gradient again. There are a few similar
+             * ways of doing this, with this one being the simplest of those
+             * ways. (in our humble opinion)
+             */
+            case R.id.action_refresh:
+                mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+                mNumbersList.setAdapter(mAdapter);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
 }
